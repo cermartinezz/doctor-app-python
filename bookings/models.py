eb9028c2b58d2617819c674b549daa87e3eabe5c
César Martinez <9775263+cermartinezz@users.noreply.github.com>
2025-01-13 15:10:07 -0600

    add models

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+from doctors.models import Doctor
+from patients.models import Patient
+
+
+class Appointment(Model):
+    patient = models.ForeignKey(Patient, related_name='appointments', on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, related_name='appointments', on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField()
+    notes = models.TextField()
+    status = models.CharField(max_length=20)
+
+class MedicalNote(Model):
+    appointment = models.ForeignKey(Appointment, related_name='medical_notes', on_delete=models.CASCADE)
+    node = models.TextField()
+    date = models.DateField()
--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class Doctor(Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    biography = models.TextField()
+
+class Department(Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class DoctorAvailability(Model):
+    doctor = models.ForeignKey(Doctor, related_name='availabilities', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_date = models.TimeField()
+
+class MedicalNote(Model):
+    doctor = models.ForeignKey(Doctor, related_name='medical_notes', on_delete=models.CASCADE)
+    node = models.TextField()
+    date = models.DateField
