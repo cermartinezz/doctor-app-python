e354aeccaf5c3609c4c172202384b5497bb6474b
César Martinez <9775263+cermartinezz@users.noreply.github.com>
2025-01-15 09:46:33 -0600

    add delete functionality

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,4 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -19,20 +20,27 @@ def patient_list_view(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def patient_detail_view(request, id):
+    patient = Patient.objects.filter(id=id).first()
+    if patient is None:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
     if request.method == 'GET':
-        patient = Patient.objects.filter(id=id).first()
-        if patient is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = PatientSerializer(patient)
         return Response(serializer.data)
 
     if request.method == 'PUT':
-        patient = Patient.objects.filter(id=id).first()
-        if patient is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+
         serializer = PatientSerializer(patient, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'DELETE':
+        patient.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
